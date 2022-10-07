629508f2e770ef1aea597d0f72ede2fd7be97809
Towhid_H <towhidhussain4@gmail.com>
2022-10-08 00:11:30 +0600

    first commit

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv").config();
-
+var cookieParser = require('cookie-parser')
+const cors = require("cors");
+const path = require("path");
+const helmet = require("helmet")
+ 
 const PORT = process.env.PORT || 8000;
 
 const {error} = require("./middleware/error");
@@ -9,19 +13,30 @@ const Auth = require("./Router/Auth");
 const Job = require("./Router/Jobs");
 const connect = require("./db/connection");
 const {notFound} = require("./middleware/not_found");
+const {authenticate} = require("./middleware/authenticate");
 
 
 
-
-//build in middlewares;
+// //build in middlewares;
+app.use(helmet.frameguard({ action: 'DENY' }));
+app.use(express.static(path.join(__dirname , "./build")))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname ,  './build/index.html'));
+  });
+app.use(cookieParser())
 app.use(express.urlencoded({extended : false}));
 app.use(express.json());
-app.use(express.urlencoded({extended : false}));
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}
+));
+
 
 
 //Apps middlewares;
 app.use(Auth);
-app.use( "/job" , Job);
+app.use(  "/job" ,authenticate , Job);
 app.use(error);
 app.use("*" , notFound);
 
@@ -30,7 +45,7 @@ async function start(){
  try {
      await  connect(process.env.DB)
     app.listen(PORT , ()=>{
-        console.log(`http://localhost:${PORT}`);
+        console.log(`http://127.0.0.1:${PORT}`);
     })
  } catch (error) {
   console.log(error);  
---
+++ build/precache-manifest.bd15d79a1a04c1d86678b03dfec412bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b61620eff7c2df82bb5e",
+    "url": "/static/css/main.2916d2ce.chunk.css"
+  },
+  {
+    "revision": "b61620eff7c2df82bb5e",
+    "url": "/static/js/main.b61620ef.chunk.js"
+  },
+  {
+    "revision": "70ade6f1e2919d18e37d",
+    "url": "/static/js/1.70ade6f1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c947a481ae006452a7d3afa0d7463fe2",
+    "url": "/static/media/loading.c947a481.png"
+  },
+  {
+    "revision": "09260f62e0f42796407c9122a02a9430",
+    "url": "/index.html"
+  }
+];
